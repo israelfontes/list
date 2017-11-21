e7a34731a9a42b2fdb6989c9db0a6e92275ca470
israelfontes <israeljapi@gmail.com>
2017-11-21 12:11:06 -0300

    bug fix in iterator

--- src/teste.cpp
+++ src/teste.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <string>
-#include "include/list.hpp"
+#include "../include/list.hpp"
 using namespace sc;
-
+using namespace std;
 int main(void){
 	list<std::string> in;
-	in.push_front("oi");
-	in.push_front("oii");
-	in.push_front("israel");
+	in.push_front("one");
+	in.push_front("two");
+	in.push_front("three");
+	auto it = in.begin();
 	
+	cout << *(--it) << endl;
+
+	in.pop_back();
+	in.pop_front();
 	std::cout << in.back() << std::endl;
 	std::cout << in.front() << std::endl;
+	cout << in.size() << endl;
 	return 0;
 }
